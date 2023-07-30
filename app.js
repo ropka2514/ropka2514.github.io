0f3acda8262a47f23fd71aaea897777ae2374b0a
ropka2514 <kristenbrun@hotmail.com>
2023-07-30 22:48:02 +0200

    update

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
+document.selectedView = '';
+
 function changeView(viewId) {
-    const contentDiv = document.getElementById('content');
-    fetch(`views.html`)
-        .then(response => response.text())
-        .then(data => {
-            let parser = new DOMParser();
-            let htmlDoc = parser.parseFromString(data, 'text/html');
-            let view = htmlDoc.getElementById(viewId);
-            contentDiv.innerHTML = view.innerHTML;
-        });
+    if (document.selectedView === viewId) {
+        return;
+    }
+    const target = document.getElementById(viewId);
+    const menu = document.getElementById('menu');
+    const current = document.getElementById(document.selectedView);
+    // displayView(viewId);
+    if (document.selectedView === '') {
+        target.classList.add('selected');
+        target.classList.remove('hidden');
+        document.selectedView = viewId;
+        return;
+    }
+    document.selectedView = viewId;
+    current.classList.remove('selected');
+    current.classList.add('hidden');
+    
+   
 }
+// function displayView(viewId){
+//     const contentDiv = document.getElementById('content');
+//     fetch(`views.html`)
+//         .then(response => response.text())
+//         .then(data => {
+//             let parser = new DOMParser();
+//             let htmlDoc = parser.parseFromString(data, 'text/html');
+//             let view = htmlDoc.getElementById(viewId);
+//             contentDiv.innerHTML = view.innerHTML;
+//         });
+// }
